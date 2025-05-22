76abe27f98ea49bad52b2f8906806a7f5beec48f
Aseem Sethi <aseemsethi@yahoo.com>
2025-05-22 17:36:53 +0530

    some changes

--- lib/repository.py
+++ lib/repository.py
@@ -3,7 +3,29 @@ import subprocess
 import sys
 import shutil
 
-def download_github_repo(repo_url, destination_foler):
+def download_github_repo(repo_url, destination_folder):
     #Check if repo exists
     print("Download Github repo..")
     
+    # Check if the repository already exists
+    if os.path.exists(destination_folder):
+        print(f"Repository already exists at {destination_folder}. Skipping download.")
+        return
+
+    # Ensure git is installed
+    if not shutil.which("git"):
+        print("Git is not installed. Please install Git and try again.")
+        sys.exit(1)
+    
+    os.makedirs(destination_folder, exist_ok=True)
+
+    # Construct the git clone command
+    command = ["git", "clone", repo_url, destination_folder]
+    try:
+        # Execute the git clone command
+        subprocess.run(command, check=True)
+        print(f"Repository cloned into {destination_folder}")
+        #init_git_command = ["cd", destination_folder, "&&", "git", "init"]
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while cloning the repository: {e}")
+        sys.exit(1)
--- main.py
+++ main.py
@@ -17,10 +17,40 @@ set_verbose(True)
 load_dotenv()
 
 def main():
-    print("Sectool...", flush=True)
-    #prompt the user to select a model
+    print("\nSectool........", flush=True)
+    #Parse command line args
+    # args - github URL that we would like to check
+    parser = argparse.ArgumentParser(description="GitHub Repo QA CLI Application")
+    parser.add_argument("repo_url", type=str, help="URL of GitHub repo")
+    args = parser.parse_args()
+    print(f"Github URL : {args.repo_url}")
+
+    # Prompt the user to select a model
+    # Our models are locally behind ollama. Run ollama run <model>
+    # to run the models and access using REST API
     model_name = select_model()
     model_info = MODELS_MAP[model_name]
-    
+
+    # Extract the repo name from the GitHub URL passed as params
+    repo_name = args.repo_url.split("/")[-1].replace(".git","")
+    print(f"repo_name: {repo_name}")
+
+
+    # We have data/ in the .ignore files, so these downloads do not get committed.
+    # Get path to data folder from URL https://github.com/aseemsethi/scraper.git
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    #/Users/aseemsethi/aseem/secTool
+    repo_dir = os.path.join(base_dir, "data", repo_name) 
+    # /Users/aseemsethi/aseem/secTool/data/scraper
+    db_dir = os.path.join(base_dir, "data", "db") 
+    # /Users/aseemsethi/aseem/secTool/data/db
+    print (repo_dir, db_dir)
+    prompt_templates_dir = os.path.join(base_dir, "prompt_templates")
+    #/Users/aseemsethi/aseem/secTool/prompt_templates
+
+    # Download the github repo
+    print(f"Downloading repo from {args.repo_url}")
+    download_github_repo(args.repo_url, repo_dir)
+
 if __name__ == "__main__":
     main()
